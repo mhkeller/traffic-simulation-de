4a150bc8d5aab1579a88efc703a523b25c9ebe66
mhkeller52 <mkeller52@bloomberg.net>
2016-10-20 14:26:21 -0400

    tabs -> spaces; standard style

--- js/canvasresize.js
+++ js/canvasresize.js
@@ -1,20 +1,20 @@
-//############################################
-// resize canvas to fit client dimension on initialization 
+// ############################################
+// resize canvas to fit client dimension on initialization
 // or whenever client size changed (e.g. rotating on smartphones)
 // responsive design
-// aspect ratio (width/heigh denotes limits where width or height 
+// aspect ratio (width/heigh denotes limits where width or height
 // is limit to increase scale
 // needs document.getElementById("mainTable")
 // returns if canvas size has changed
-//############################################
+// ############################################
 
-function canvas_resize(canvas,aspectRatio){
-    var hasChanged=false;
+function canvas_resize (canvas, aspectRatio) {
+  var hasChanged = false
 
-// get dimensions/position of the main table above the canvas 
+// get dimensions/position of the main table above the canvas
 // relative to the global document.body element <body>...</body>
 
-    var tab=document.getElementById("mainTable");
+  var tab = document.getElementById('mainTable')
 
     // access frames:  window=simwindow with frames
     // just window (obviously standard name?) w/o frames => window!
@@ -23,58 +23,56 @@ function canvas_resize(canvas,aspectRatio){
     var navwindow = parent.navigation;
 
     console.log("document.body.offsetWidth=",document.body.offsetWidth,
-		"document.body.offsetHeight=",document.body.offsetHeight,
-		" window.innerWidth=",window.innerWidth,
-		" window.innerHeight=",window.innerHeight,
-		" simwindow.innerWidth=",simwindow.innerWidth,
-		" simwindow.innerHeight=",simwindow.innerHeight,
-		" navwindow.innerWidth=",navwindow.innerWidth,
-		" navwindow.innerHeight=",navwindow.innerHeight);
+    "document.body.offsetHeight=",document.body.offsetHeight,
+    " window.innerWidth=",window.innerWidth,
+    " window.innerHeight=",window.innerHeight,
+    " simwindow.innerWidth=",simwindow.innerWidth,
+    " simwindow.innerHeight=",simwindow.innerHeight,
+    " navwindow.innerWidth=",navwindow.innerWidth,
+    " navwindow.innerHeight=",navwindow.innerHeight);
 */
 
-
-   // height of actual simulation; 
-    // restrict upper navig eleents to 30% of total height! set it to at 
-    var yPixCanvasTop=tab.getBoundingClientRect().bottom
-    yPixCanvasTop=Math.min(yPixCanvasTop,0.3*window.innerHeight);
+   // height of actual simulation;
+    // restrict upper navig eleents to 30% of total height! set it to at
+  var yPixCanvasTop = tab.getBoundingClientRect().bottom
+  yPixCanvasTop = Math.min(yPixCanvasTop, 0.3 * window.innerHeight)
 
     // document.body.offsetWidth smaller than window.innerWidth
     // cannot use window.innerWidth because of scrollbars
 
-    var newWidth=Math.round(document.body.offsetWidth); 
-    var newHeight=Math.round(window.innerHeight-yPixCanvasTop); 
+  var newWidth = Math.round(document.body.offsetWidth)
+  var newHeight = Math.round(window.innerHeight - yPixCanvasTop)
 
-    //console.log("yPixCanvasTop=",yPixCanvasTop,
-//		" window.innerHeight=",window.innerHeight);
+    // console.log("yPixCanvasTop=",yPixCanvasTop,
+//    " window.innerHeight=",window.innerHeight);
 
-    if (canvas.width!=newWidth){
-	hasChanged=true;
-	canvas.width  = newWidth;
-    }
+  if (canvas.width != newWidth) {
+    hasChanged = true
+    canvas.width = newWidth
+  }
 
-    if (canvas.height != newHeight){
-	hasChanged=true;
-        canvas.height  = newHeight;
-    }
+  if (canvas.height != newHeight) {
+    hasChanged = true
+    canvas.height = newHeight
+  }
 
-    if(false){
-      console.log(" canvas.width=",canvas.width);
-      console.log(" canvas.height=",window.innerHeight);
+  if (false) {
+    console.log(' canvas.width=', canvas.width)
+    console.log(' canvas.height=', window.innerHeight)
 
-      console.log("document.body.offsetWidth=",document.body.offsetWidth);
-      console.log("document.body.offsetHeight=",document.body.offsetHeight);
-    }	
+    console.log('document.body.offsetWidth=', document.body.offsetWidth)
+    console.log('document.body.offsetHeight=', document.body.offsetHeight)
+  }
 
-
-    if(hasChanged){
-        center_x=0.50*canvas.width; // pixel coordinates
-        center_y=0.48*canvas.height;
-	var refDim=Math.min(canvas.width,canvas.height*aspectRatio);
+  if (hasChanged) {
+    center_x = 0.50 * canvas.width // pixel coordinates
+    center_y = 0.48 * canvas.height
+    var refDim = Math.min(canvas.width, canvas.height * aspectRatio)
 
         // global scale pixel/m;
-	scale=refDim/sizePhys;  
-	//console.log("scale=",scale," hasChanged=",hasChanged);
-    }
-    return hasChanged;
+    scale = refDim / sizePhys
+  // console.log("scale=",scale," hasChanged=",hasChanged);
+  }
+  return hasChanged
 }
 
--- js/models.js
+++ js/models.js
@@ -1,73 +1,71 @@
-//#################################
+// #################################
 // longitudinal model
-//#################################
+// #################################
 
-function IDM(v0,T,s0,a,b){
-    this.v0=v0; 
-    this.T=T;
-    this.s0=s0;
-    this.a=a;
-    this.b=b;
-    this.alpha_v0=1; // multiplicator for temporary reduction
+function IDM (v0, T, s0, a, b) {
+  this.v0 = v0
+  this.T = T
+  this.s0 = s0
+  this.a = a
+  this.b = b
+  this.alpha_v0 = 1 // multiplicator for temporary reduction
 
     // possible restrictions (value 1000 => initially no restriction)
-    this.speedlimit=1000; // if effective speed limits, speedlimit<v0  
-    this.speedmax=1000; // if vehicle restricts speed, speedmax<speedlimit, v0
-    this.bmax=16;
-    this.calcAcc=function(s,v,vl){
-    //IDM.prototype.calcAcc=function(s,v,vl){ // this works as well but less intuitive
+  this.speedlimit = 1000 // if effective speed limits, speedlimit<v0
+  this.speedmax = 1000 // if vehicle restricts speed, speedmax<speedlimit, v0
+  this.bmax = 16
+  this.calcAcc = function (s, v, vl) {
+    // IDM.prototype.calcAcc=function(s,v,vl){ // this works as well but less intuitive
 
-	//if(s<0.1){console.log("IDM.calcAcc: s="+s+" < approx 0!<br>");}
+  // if(s<0.1){console.log("IDM.calcAcc: s="+s+" < approx 0!<br>");}
 
-        //MT 2016: noise 
-	var noiseAcc=0.3; // sig_speedFluct=noiseAcc*sqrt(t*dt/12)
-        var accRnd=noiseAcc*(Math.random()-0.5); //if acceleration noise
+        // MT 2016: noise
+    var noiseAcc = 0.3 // sig_speedFluct=noiseAcc*sqrt(t*dt/12)
+    var accRnd = noiseAcc * (Math.random() - 0.5) // if acceleration noise
 
-	var v0eff=Math.min(this.v0, this.speedlimit, this.speedmax);
-	v0eff*=this.alpha_v0;
-       	var accFree=(v<v0eff) ? this.a*(1-Math.pow(v/v0eff,4))
-	    : this.a*(1-v/v0eff);
-	var sstar=this.s0+v*this.T+0.5*v*(v-vl)/Math.sqrt(this.a*this.b);
-	//var sstar=this.s0+v*this.T+0.7*Math.pow(v,0.5)*(v-vl)*Math.pow(Math.abs(v-vl),0.3)/Math.sqrt(this.a*this.b);
-	var accInt=-this.a*Math.pow(sstar/Math.max(s,this.s0),2);
-	var accInt_IDMplus=accInt+this.a;
-	//if(false){
-	//if(this.speedlimit<23){
-	if(this.alpha_v0<0.6){
-          console.log("IDM.calcAcc:"
-	    +" speedlimit="+this.speedlimit
-	    +" speed="+v
-	    +" accFree="+accFree
-	    +" acc="+Math.max(-this.bmax, accFree + accInt + accRnd));
-	}
-	return (v0eff<0.00001) ? 0 : Math.max(-this.bmax, accFree + accInt + accRnd);//IDM
-	//return Math.max(-this.bmax, Math.min(accFree, accInt_IDMplus));//IDMplus
+    var v0eff = Math.min(this.v0, this.speedlimit, this.speedmax)
+    v0eff *= this.alpha_v0
+    var accFree = (v < v0eff) ? this.a * (1 - Math.pow(v / v0eff, 4))
+      : this.a * (1 - v / v0eff)
+    var sstar = this.s0 + v * this.T + 0.5 * v * (v - vl) / Math.sqrt(this.a * this.b)
+  // var sstar=this.s0+v*this.T+0.7*Math.pow(v,0.5)*(v-vl)*Math.pow(Math.abs(v-vl),0.3)/Math.sqrt(this.a*this.b);
+    var accInt = -this.a * Math.pow(sstar / Math.max(s, this.s0), 2)
+    var accInt_IDMplus = accInt + this.a
+  // if(false){
+  // if(this.speedlimit<23){
+    if (this.alpha_v0 < 0.6) {
+      console.log('IDM.calcAcc:'
+      + ' speedlimit=' + this.speedlimit
+      + ' speed=' + v
+      + ' accFree=' + accFree
+      + ' acc=' + Math.max(-this.bmax, accFree + accInt + accRnd))
     }
+    return (v0eff < 0.00001) ? 0 : Math.max(-this.bmax, accFree + accInt + accRnd)// IDM
+  // return Math.max(-this.bmax, Math.min(accFree, accInt_IDMplus));//IDMplus
+  }
 }
 
-//#################################
+// #################################
 // lane-changing models
-//#################################
+// #################################
 
 /**
 generalized lane-changing model MOBIL:
-at present no politeness but speed dependent safe deceleration 
+at present no politeness but speed dependent safe deceleration
 
 @param bSafe:      safe deceleration [m/s^2] at maximum speed v=v0
 @param bSafeMax:   safe deceleration [m/s^2]  at speed zero (gen. higher)
-@param bThr:       lane-changing threshold [m/s^2] 
+@param bThr:       lane-changing threshold [m/s^2]
 @param bBiasRight: bias [m/s^2] to the right
 @return:           MOBIL instance (constructor)
 */
 
-function MOBIL(bSafe, bSafeMax, bThr, bBiasRight){
-    this.bSafe=bSafe;
-    this.bThr=bThr;
-    this.bBiasRight=bBiasRight;
-    this.bSafeMax=bSafeMax; //!!! transfer into arg list of cstr later on
-    //this.p=p; 
-
-
+function MOBIL (bSafe, bSafeMax, bThr, bBiasRight) {
+  this.bSafe = bSafe
+  this.bThr = bThr
+  this.bBiasRight = bBiasRight
+  this.bSafeMax = bSafeMax //! !! transfer into arg list of cstr later on
+    // this.p=p;
 
     /**
     generalized MOBIL lane chaning decision
@@ -82,27 +80,26 @@ function MOBIL(bSafe, bSafeMax, bThr, bBiasRight){
     @return: whether an immediate lane change is safe and desired
     */
 
-    MOBIL.prototype.realizeLaneChange=function(vrel,acc,accNew,accLagTargetNew,toRight,log){
-
- 	if(log|| (this.bSafe>24)){
-          console.log("\nIn MOBIL.realizeLaneChange");
-	  console.log("  vrel="+vrel
-	              +" acc="+acc+" accNew="+accNew
-		      +" accLagTargetNew="+accLagTargetNew);
-	  console.log("  toRight="+toRight);
-	  console.log("  this.bSafe="+this.bSafe+" this.bThr="+this.bThr
-		      +" this.bBiasRight="+this.bBiasRight);
-	}
-
-	var bSafeActual=vrel*this.bSafe+(1-vrel)*this.bSafeMax;
-
-	if(accLagTargetNew<-bSafeActual){return false;}
-	var dacc=accNew-acc + this.bBiasRight*((toRight) ? 1 : -1)- this.bThr;
-
-	if(log || (this.bSafe>24)){
-	  console.log("...dacc="+dacc);
-	  if(dacc>0){console.log("  positive MOBIL LC decision!");}
-	}	
-	return (dacc>0);
+  MOBIL.prototype.realizeLaneChange = function (vrel, acc, accNew, accLagTargetNew, toRight, log) {
+    if (log || (this.bSafe > 24)) {
+      console.log('\nIn MOBIL.realizeLaneChange')
+      console.log('  vrel=' + vrel
+                + ' acc=' + acc + ' accNew=' + accNew
+          + ' accLagTargetNew=' + accLagTargetNew)
+      console.log('  toRight=' + toRight)
+      console.log('  this.bSafe=' + this.bSafe + ' this.bThr=' + this.bThr
+          + ' this.bBiasRight=' + this.bBiasRight)
     }
+
+    var bSafeActual = vrel * this.bSafe + (1 - vrel) * this.bSafeMax
+
+    if (accLagTargetNew < -bSafeActual) { return false }
+    var dacc = accNew - acc + this.bBiasRight * ((toRight) ? 1 : -1) - this.bThr
+
+    if (log || (this.bSafe > 24)) {
+      console.log('...dacc=' + dacc)
+      if (dacc > 0) { console.log('  positive MOBIL LC decision!') }
+    }
+    return (dacc > 0)
+  }
 }
